0dca9ed47cab0615ac1ad4e56baf3c74ca8f6685
justbattery <pstarzyk5@gmail.com>
2023-09-19 21:02:38 +0200

    Fixed bug with foreach function not working
    Now forech that controls if drink is added to list TodaysDrink is working properly

--- CaffCalc/DailyIntakeFunctions/AddDrinkToList.cs
+++ CaffCalc/DailyIntakeFunctions/AddDrinkToList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using static CaffCalc.CodeBehind.BackendDB;
 
 namespace CaffCalc.DailyIntakeFunctions
@@ -13,12 +14,15 @@ namespace CaffCalc.DailyIntakeFunctions
         {
             System.Diagnostics.Debug.WriteLine($"Test started");
 
+            TodaysDrinks drink3 = new TodaysDrinks { Name = "TEST", Count = 3 };
+            TodaysDrinksList.Add(drink3);
+
             foreach (TodaysDrinks testDrink in TodaysDrinksList)
             {
                 System.Diagnostics.Debug.WriteLine($"Test stage 1");
                 if (choosenDrink.Name == testDrink.Name) // CO JEŚLI NAZWA TA SAMA
                 {
-                    System.Diagnostics.Debug.WriteLine($"PLUH");
+                    System.Diagnostics.Debug.WriteLine($"Route 1");
                     int count = testDrink.Count + 1;
                     TodaysDrinks drink = new TodaysDrinks { Name = choosenDrink.Name, Count = count };
                     int index = TodaysDrinksList.IndexOf(testDrink);
@@ -28,7 +32,7 @@ namespace CaffCalc.DailyIntakeFunctions
                 }
                 else
                 {
-                    System.Diagnostics.Debug.WriteLine($"plug");
+                    System.Diagnostics.Debug.WriteLine($"Route 2");
                     int count = 1;
                     TodaysDrinks drink = new TodaysDrinks { Name = choosenDrink.Name, Count = count };
                     TodaysDrinksList.Add(drink);
@@ -36,29 +40,7 @@ namespace CaffCalc.DailyIntakeFunctions
                 }
             }
 
-            //foreach (TodaysDrinks testDrink in TodaysDrinksList)
-            //{
-            //    System.Diagnostics.Debug.WriteLine($"Test stage 1");
-            //    if (choosenDrink.Name == testDrink.Name) // CO JEŚLI NAZWA TA SAMA
-            //    {
-            //        System.Diagnostics.Debug.WriteLine($"PLUH");
-            //        int count = testDrink.Count + 1;
-            //        TodaysDrinks drink = new TodaysDrinks { Name = choosenDrink.Name, Count = count };
-            //        int index = TodaysDrinksList.IndexOf(testDrink);
-            //        TodaysDrinksList.RemoveAt(index);
-            //        TodaysDrinksList.Insert(index, drink);
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        System.Diagnostics.Debug.WriteLine($"plug");
-            //        int count = 1;
-            //        TodaysDrinks drink = new TodaysDrinks { Name = choosenDrink.Name, Count = count };
-            //        TodaysDrinksList.Add(drink);
-            //        break;
-            //    }
-            //}
-            System.Diagnostics.Debug.WriteLine($"DA PLUH IS GOOD");
+            System.Diagnostics.Debug.WriteLine($"Test ended");
             SaveStatsToDictionary();
             SaveStatsToFile();
         }
